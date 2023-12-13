38484f50e57cc4b5d42515f5ee21c65f9d56fb84
Nontapat <65050434@kmitl.ac.th>
2023-12-13 15:06:29 +0700

    add files Day13.java and input13.txt

---
+++ Day13.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day13 {
+    public static void main(String[] args) throws IOException {
+        List<List<String>> list = new ArrayList<>();
+        List<String> in = Files.lines(Paths.get("input13.txt")).collect(Collectors.toList());
+        List<String> map = new ArrayList<>();
+        list.add(map);
+        for (String line : in) {
+            if (!line.isEmpty()) {
+                map.add(line);
+            } else {
+                map = new ArrayList<>();
+                list.add(map);
+            }
+        }
+
+        int sum = 0;
+        for (List<String> m : list) {
+            for (int i = 1; i < m.get(0).length(); i++) {
+                if (colReflect(m, i)) {
+                    int x = i;
+                    sum += x;
+                }
+            }
+            for (int i = 1; i < m.size(); i++) {
+                if (rowReflect(m, i)) {
+                    int x = 100*i;
+                    sum += x;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    static boolean colReflect(List<String> map, int c) {
+        int min = Math.min(c, map.get(0).length()-c);
+        for (String row : map) {
+            String left = row.substring(c-min, c);
+            String right = new StringBuilder(row.substring(c, c+min)).reverse().toString();
+            if (!left.equals(right)) return false;
+        }
+        return true;
+    }
+
+    static boolean rowReflect(List<String> map, int r) {
+        int min = Math.min(r, map.size()-r);
+        int cols = map.get(0).length();
+        for (int i = 0; i < cols; i++) {
+            int j = i;
+            String col = map.stream().map(e -> String.valueOf(e.charAt(j)))
+                .collect(Collectors.joining());
+            String up = col.substring(r-min, r);
+            String down = new StringBuilder(col.substring(r, r+min)).reverse().toString();
+            if (!up.equals(down)) return false;
+        }
+        return true;
+    }
+}
